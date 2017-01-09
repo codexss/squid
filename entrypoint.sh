ffcd562d2380d291ca56906ffd9c288243441e2c
Lentin <codexss@users.noreply.github.com>
2017-01-09 18:35:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ "$HOSTNAME" != "" ]; then
-  printf "visible_hostname $HOSTNAME" >> /etc/squid/squid.conf
+  echo "visible_hostname $HOSTNAME" >> /etc/squid/squid.conf
 fi
 squid -NX
